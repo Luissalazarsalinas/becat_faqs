234b769ac8fcd45c58c72a09ac87e6e0bde4ff27
Luissalazarsalinas <lf.salazarsalinas@gmail.com>
2024-12-12 19:59:31 -0500

    updated code

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN pip install --no-cache-dir --upgrade pip setuptools wheel
 COPY ./requirements.txt /src/requirements.txt
 
 # install requirements
-RUN pip install --no-cache-dir  --upgrade -r /src/requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy code 
 COPY . .
--- src/api/workflow/faqs_workflow.py
+++ src/api/workflow/faqs_workflow.py
@@ -3,6 +3,7 @@ from langchain.base_language import BaseLanguageModel
 from langchain_core.runnables import Runnable
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage
+from tenacity import retry, wait_exponential, retry_if_exception_type, retry_if_exception_message, stop_after_attempt
 from src.api.workflow.utils.faqs_extract import FAQsExtract
 from src.api.workflow.utils.prompts import Prompts
 
@@ -37,29 +38,37 @@ class FaqsWorkflow:
         chain = prompt | llm
 
         return chain
-
+    
+    @retry(
+            stop= stop_after_attempt(3), # intenta maximo tres veces si ocurre algun fallo
+            wait= wait_exponential(multiplier=1, min=4, max=10), # espera minimo 4 segundos y maximo 10 segundos
+            retry= retry_if_exception_message(match=".*"), # los reintentos se activan con cualquier mensaje de error
+    )
     def ai_faqs(self, question:str) -> str:
 
-        # Get faqs from  becat
-        faqs:str = self.get_faqs.get_faqs()
-        logger.info("FAQs were obtained with a successful")
+        try:
+            # Get faqs from  becat
+            faqs:str = self.get_faqs.get_faqs()
+            logger.info("FAQs were obtained with a successful")
 
-        # format the system prompt 
-        system_prompt:str = Prompts.prompt(faqs=faqs, question=question)
-        # TODO: add a logging message
+            # format the system prompt 
+            system_prompt:str = Prompts.prompt(faqs=faqs, question=question)
+            # TODO: add a logging message
 
-        # chain
-        chain = self.__chain(
-            llm=self.llm,
-            system_prompt=system_prompt
-        )
+            # chain
+            chain = self.__chain(
+                llm=self.llm,
+                system_prompt=system_prompt
+            )
 
-        # run chain
-        logger.debug(f"Processing query: {question}")
-        results = chain.invoke({"messages": [HumanMessage(content=self._task)]})
-        logger.info("The query was processed with a successful")
+            # run chain
+            logger.debug(f"Processing query: {question}")
+            results = chain.invoke({"messages": [HumanMessage(content=self._task)]})
+            logger.info("The query was processed with a successful")
 
-        return results.content
-
+            return results.content
+        except Exception as e:
+            print(f"Any error has ocurred: {e}")
+            raise 
 
         
